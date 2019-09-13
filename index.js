d0de007dcbceab027bd464464b439e8be0f361a6
Pedro Amaro <n101010k@gmail.com>
2019-09-13 23:45:57 +0100

    Improved autosaver, added autoloader
    Added a %date field to autosaves, so that no autosaves are overwritten.
    Added a way of setting the autosave message.
    Added an autoloader that loads the last autosave saved when a server starts.

--- index.js
+++ index.js
@@ -8,13 +8,51 @@ try {
 } catch(e) {
 	conf = {
 		intervalInSeconds: 300,
-		saveName: 'autosave'
+		saveName: 'autosave_%date',
+		saveMessage: 'Autosaved bricks (if any).'
 	};
 
 	fs.writeFileSync(confPath, JSON.stringify(conf, null, 2));
 }
 
-setInterval(() => {
-	global.Brikkit.saveBricks(conf.saveName);
-	global.Brikkit.say('Autosaved bricks (if any).');
-}, conf.intervalInSeconds * 1000);
+function autosave() {
+    // replace %date with the current date, as an ISO8601 date
+    const isoDate = (new Date()).toISOString();
+    const finalSaveName = conf.saveName.replace('%date', isoDate);
+
+    global.Brikkit.saveBricks(finalSaveName);
+    global.Brikkit.say(conf.saveMessage);
+}
+
+const dateRegex = `\\d\\d\\d\\d-\\d\\d-\\d\\dT\\d\\d_\\d\\d_\\d\\d\\.\\d\\d\\dZ`;
+global.Brikkit.on('start', evt => {
+    global.Brikkit.getSaves(saves => {
+        // create a regex for matching the autosave file (include %date)
+        const regexString = `^${conf.saveName.replace('%date', dateRegex)}$`;
+        const regex = new RegExp(regexString);
+        
+        // find the latest file that corresponds to the regex
+        let latestSave = undefined;
+        for(let i = 0; i < saves.length; ++i) {
+            const save = saves[i];
+            
+            if(!regex.test(save))
+                continue;
+            
+            if(latestSave === undefined) {
+                latestSave = save;
+                continue;
+            }
+            
+            if(save > latestSave)
+                latestSave = save;
+        }
+        
+        // if a file was found, load it
+        if(latestSave !== undefined)
+            global.Brikkit.loadBricks(latestSave);
+        
+        // start autosaving after we load the save
+        setInterval(autosave, conf.intervalInSeconds * 1000);
+    });
+});
